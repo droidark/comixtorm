093f74c337a64447e82236e6157b3864039b7740
Fozz <droidark@outlook.com>
2018-09-09 21:36:23 -0500

    Add and delete item in the collection, COMPLETE

--- src/main/java/com/comixtorm/collector/service/impl/UserServiceImpl.java
+++ src/main/java/com/comixtorm/collector/service/impl/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package com.comixtorm.collector.service.impl;
 
 import com.comixtorm.collector.dto.PublisherDto;
-import com.comixtorm.collector.dto.TitleDto;
 import com.comixtorm.collector.dto.UserDto;
 import com.comixtorm.collector.exception.UserAlreadyExistException;
 import com.comixtorm.collector.model.*;
@@ -37,15 +36,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void save(UserDto userDto) throws UserAlreadyExistException {
-//        User user = userRepository.findByUsername(userDto.getUsername());
-//        if (user == null) {
-//            userDto.setSignUpDate(new Date());
-//            userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
-//            userDto.setStatus("PENDING");
-//            userRepository.save(converterService.convertToUser(userDto, true, false, false, false));
-//        } else {
-//            throw new UserAlreadyExistException("User already exist!");
-//        }
+        User user = userRepository.findByUsername(userDto.getUsername());
+        if (user == null) {
+            userDto.setSignUpDate(new Date());
+            userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
+            userDto.setStatus("PENDING");
+            userRepository.save(converterService.toUser(userDto));
+        } else {
+            throw new UserAlreadyExistException("User already exist!");
+        }
     }
 
     @Override
